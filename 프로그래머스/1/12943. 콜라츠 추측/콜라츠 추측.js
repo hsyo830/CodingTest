b98d6b4493c85fdad819fb4c5f9cdce76745f53d
SeoYoung Hwang <nct830@naver.com>
2026-03-18 00:31:10 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,23 @@
+function solution(num) {
+  let count = 0;
+
+  if (num === 1) {
+    return 0;
+  }
+
+  for (let i = 0; i <= 500; i++) {
+    if (num % 2 === 0) {
+      num = num / 2;
+      count++;
+    } else {
+      num = num * 3 + 1;
+      count++;
+    }
+    if (num === 1) {
+      return count;
+    }
+    if (i === 500) {
+      return -1;
+    }
+  }
+}
