1b8c94b854fa7cd1d2e7565d024bca2bd93967b6
SeoYoung Hwang <nct830@naver.com>
2026-04-07 23:48:16 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -0,0 +1,10 @@
+function solution(binomial) {
+  let result = binomial.split(" ");
+  if (result[1] === "+") {
+    return Number(result[0]) + Number(result[2]);
+  } else if (result[1] === "-") {
+    return Number(result[0]) - Number(result[2]);
+  } else {
+    return Number(result[0]) * Number(result[2]);
+  }
+}
